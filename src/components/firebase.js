664e79f5ee558ae34cc5320a39f71927b4423e83
Praveensk06 <113981543+Praveensk06@users.noreply.github.com>
2023-03-11 19:22:53 +0530

    add firebase setup

---
+++ src/components/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore"; // Import any Firebase services you plan to use
+
+const firebaseConfig = {
+  const firebaseConfig = {
+  apiKey: "AIzaSyC1KzTxxXYG2KHcaYWNMTTHScLX6TN2PUI",
+  authDomain: "codepen-clone-86816.firebaseapp.com",
+  projectId: "codepen-clone-86816",
+  storageBucket: "codepen-clone-86816.appspot.com",
+  messagingSenderId: "715239313251",
+  appId: "1:715239313251:web:ab4a7c72ba183493cdcd56"
+};
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
